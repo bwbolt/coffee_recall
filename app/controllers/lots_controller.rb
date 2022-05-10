6eb7f4d973079727d5d56bbfff18df0de268769d
Bryce Wein <bryce.wein@gmail.com>
2022-05-09 21:21:41 -0400

    Feat: add update method for lots

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -21,4 +21,17 @@ class LotsController < ApplicationController
   def edit
     @lot = Lot.find(params[:id])
   end
+
+  def update
+    lot = Lot.find(params[:id])
+    lot.update({
+                 name: params[:name],
+                 lot_number: params[:lot_number],
+                 importer: params[:importer],
+                 organic: params[:organic]
+               })
+    lot.save
+
+    redirect_to "/lots/#{lot.id}"
+  end
 end
