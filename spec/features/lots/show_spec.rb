76422d4905b16f916b8e6dfaccc12a79fcc9c220
Bryce Wein <bryce.wein@gmail.com>
2022-05-11 20:41:22 -0400

    Test: add test for number of bags associated

--- spec/features/lots/show_spec.rb
+++ spec/features/lots/show_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe 'lots#show', type: :feature do
     expect(page).to have_content(lot1.updated_at)
     expect(page).to have_content(lot1.id)
   end
+
+  it 'shows how many bags are associated with this lot' do
+    lot1 = Lot.create!(name: 'honduras', lot_number: 55, organic: false)
+    lot2 = Lot.create!(name: 'guat', lot_number: 67, organic: true)
+    bag1 = Bag.create!(roast: 'medium', ground: true, size: 340, lot_id: lot1.id)
+    bag2 = Bag.create!(roast: 'dark', ground: true, size: 60, lot_id: lot2.id)
+    bag3 = Bag.create!(roast: 'light', ground: true, size: 60, lot_id: lot1.id)
+    visit "/lots/#{lot1.id}"
+    expect(page).to have_content('Number of Bags from this lot: 2')
+    visit "/lots/#{lot2.id}"
+    expect(page).to have_content('Number of Bags from this lot: 1')
+  end
 end
