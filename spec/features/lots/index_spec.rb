d271f78b401fec45537111e9136634b2944770d8
Bryce Wein <bryce.wein@gmail.com>
2022-05-11 20:57:11 -0400

    Test: add test that there is a link on every page

--- spec/features/lots/index_spec.rb
+++ spec/features/lots/index_spec.rb
@@ -14,4 +14,31 @@ RSpec.describe 'lots#index', type: :feature do
     expect(lot2.name).to appear_before(lot1.name)
     expect(page).to have_content(lot1.created_at)
   end
+
+  it 'has a link to it from every page' do
+    lot1 = Lot.create!(name: 'honduras', lot_number: 55, organic: false)
+    bag = Bag.create!(roast: 'medium', ground: true, size: 340, lot_id: lot1.id)
+    visit '/'
+    expect(page).to have_link('Lots')
+    visit '/lots'
+    expect(page).to have_link('Lots')
+    visit '/lots/new'
+    expect(page).to have_link('Lots')
+    visit '/lots/new'
+    expect(page).to have_link('Lots')
+    visit "/lots/#{lot1.id}"
+    expect(page).to have_link('Lots')
+    visit "/lots/#{lot1.id}/edit"
+    expect(page).to have_link('Lots')
+    visit "/lots/#{lot1.id}/bags"
+    expect(page).to have_link('Lots')
+    visit '/bags'
+    expect(page).to have_link('Lots')
+    visit "/bags/#{bag.id}"
+    expect(page).to have_link('Lots')
+    visit "/bags/#{bag.id}/edit"
+    expect(page).to have_link('Lots')
+    visit "/lots/#{lot1.id}/bags/new"
+    expect(page).to have_link('Lots')
+  end
 end
