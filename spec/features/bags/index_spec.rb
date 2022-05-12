59871d360c919c2e115b36ebce272183d1993276
Bryce Wein <bryce.wein@gmail.com>
2022-05-11 20:17:58 -0400

    Test: add lots to bag tests

--- spec/features/bags/index_spec.rb
+++ spec/features/bags/index_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'bags#index', type: :feature do
   it 'shows all bags and attributes' do
-    bag = Bag.create!(roast: 'medium', ground: true, size: 340)
+    lot1 = Lot.create!(name: 'honduras', lot_number: 55, organic: false)
+    bag = Bag.create!(roast: 'medium', ground: true, size: 340, lot_id: lot1.id)
     visit '/bags'
     expect(page).to have_content(bag.roast)
     expect(page).to have_content(bag.created_at)
--- spec/features/bags/show_spec.rb
+++ spec/features/bags/show_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'bags#show', type: :feature do
   it 'shows a specific bag and its attributes' do
-    bag = Bag.create!(roast: 'medium', ground: true, size: 340)
+    lot1 = Lot.create!(name: 'honduras', lot_number: 55, organic: false)
+    bag = Bag.create!(roast: 'medium', ground: true, size: 340, lot_id: lot1.id)
     visit "/bags/#{bag.id}"
     expect(page).to have_content(bag.roast)
     expect(page).to have_content(bag.ground)
