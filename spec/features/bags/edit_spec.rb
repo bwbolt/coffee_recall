c99a5fa7ecd9385a4c69f5782ef4686e0e9422c3
Bryce Wein <bryce.wein@gmail.com>
2022-05-13 09:43:01 -0400

    Test: add test for edit bag link being present

--- spec/features/bags/edit_spec.rb
+++ spec/features/bags/edit_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe 'bags#edit', type: :feature do
     expect(current_path).to eq("/bags/#{bag.id}")
     expect(page).to have_content('dark')
   end
+
+  it 'there is a working link on all displayed areas' do
+    lot1 = Lot.create!(name: 'honduras', lot_number: 55, organic: false)
+    bag = Bag.create!(roast: 'medium', ground: true, size: 340, lot_id: lot1.id)
+    visit '/bags'
+    expect(page).to have_link('Update Bag')
+    click_on('Update Bag')
+    expect(current_path).to eq("/bags/#{bag.id}/edit")
+    visit "/lots/#{lot1.id}/bags"
+    expect(page).to have_link('Update Bag')
+    click_on('Update Bag')
+    expect(current_path).to eq("/bags/#{bag.id}/edit")
+  end
 end
