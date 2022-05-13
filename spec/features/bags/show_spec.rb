36e61f53ed5e0ac60fd07b2373e467fec2bc2b9b
Bryce Wein <bryce.wein@gmail.com>
2022-05-13 10:11:19 -0400

    Test: add test of bag delete button

--- spec/features/bags/show_spec.rb
+++ spec/features/bags/show_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe 'bags#show', type: :feature do
     expect(page).to have_content(bag.created_at)
     expect(page).to have_content(bag.updated_at)
   end
+
+  it 'has a working delete button' do
+    lot1 = Lot.create!(name: 'honduras', lot_number: 55, organic: false)
+    bag = Bag.create!(roast: 'Slightly Dangerous', ground: true, size: 340, lot_id: lot1.id)
+    visit '/bags'
+    expect(page).to have_content('Slightly Dangerous')
+    visit "/bags/#{bag.id}"
+    expect(page).to have_link('Delete Bag')
+    click_link('Delete Bag')
+    expect(current_path).to eq('/bags')
+    expect(page).to_not have_content('Slightly Dangerous')
+  end
 end
