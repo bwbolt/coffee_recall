1ceb994b4767a7690c20ee6e9c521baa741d5217
Bryce Wein <bryce.wein@gmail.com>
2022-05-17 16:35:00 -0400

    Feat: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bag.destroy_all
+Lot.destroy_all
+
+brazil = Lot.create!(name: 'Brazil Guaxupe Natural', importer: 'Genuine Origin', lot_number: 22, organic: false)
+mexico = Lot.create!(name: 'Mexico Veracrus Seleccion Huatusco', importer: 'Olam Specialty Coffee', lot_number: 20,
+                     organic: true)
+ethiopia = Lot.create!(name: 'Ethiopia Guji Hambela, Dame Dabaye Washing Station', importer: 'Atlas Coffee Importers',
+                       lot_number: 18, organic: true)
+
+brazil.bags.create!(name: 'Slightly Dangerous', ground: true, size: 340)
+brazil.bags.create!(name: 'Brazil Single Origin', ground: true, size: 340)
+brazil.bags.create!(name: 'Tail Wind', ground: true, size: 340)
+
+mexico.bags.create!(name: 'Blonde Bomber', ground: true, size: 60)
+mexico.bags.create!(name: 'Mexico Single Origin', ground: false, size: 340)
+mexico.bags.create!(name: 'Blonde Bomber', ground: true, size: 340)
+
+ethiopia.bags.create!(name: 'Ethiopia Single Origin', ground: true, size: 340)
+ethiopia.bags.create!(name: 'Blonde Bomber', ground: true, size: 340)
+ethiopia.bags.create!(name: 'Slightly Dangerous', ground: true, size: 60)
