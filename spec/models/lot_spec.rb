d429b2f069d8a148f207d59ca324656941137645
Bryce Wein <bryce.wein@gmail.com>
2022-05-17 14:37:21 -0400

    Test: add sort by bag count and sort by creation date tests

--- spec/models/lot_spec.rb
+++ spec/models/lot_spec.rb
@@ -22,5 +22,22 @@ RSpec.describe Lot, type: :model do
       bag2 = lot1.bags.create!(name: 'dark', ground: true, size: 60)
       expect(lot1.bags_larger_than(70)).to eq([bag1])
     end
+    it 'can sory by bag count' do
+      lot1 = Lot.create!(name: 'honduras', importer: 'lamanita', lot_number: 55, organic: false)
+      bag1 = lot1.bags.create!(name: 'medium', ground: true, size: 340)
+      bag2 = lot1.bags.create!(name: 'dark', ground: true, size: 60)
+      lot2 = Lot.create!(name: 'honduras', importer: 'lamanita', lot_number: 55, organic: false)
+      bag3 = lot2.bags.create!(name: 'medium', ground: true, size: 340)
+      expect(Lot.sort_by_bag_count).to eq([lot1, lot2])
+    end
+    it 'can sort by creation date' do
+      lot1 = Lot.create!(name: 'honduras', importer: 'lamanita', lot_number: 55, organic: false)
+      bag1 = lot1.bags.create!(name: 'medium', ground: true, size: 340)
+      bag2 = lot1.bags.create!(name: 'dark', ground: true, size: 60)
+      lot2 = Lot.create!(name: 'guat', importer: 'lamanita', lot_number: 55, organic: false)
+      bag3 = lot2.bags.create!(name: 'medium', ground: true, size: 340)
+      lot2.update(created_at: Date.yesterday)
+      expect(Lot.sort_by_creation_date).to eq([lot1, lot2])
+    end
   end
 end
