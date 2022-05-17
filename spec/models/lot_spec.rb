50510806e37e71f929a61c3d3503e74ee05e62b6
Bryce Wein <bryce.wein@gmail.com>
2022-05-17 14:40:18 -0400

    Test: add test for sorting associated bags by name

--- spec/models/lot_spec.rb
+++ spec/models/lot_spec.rb
@@ -39,5 +39,12 @@ RSpec.describe Lot, type: :model do
       lot2.update(created_at: Date.yesterday)
       expect(Lot.sort_by_creation_date).to eq([lot1, lot2])
     end
+    it 'can sort associated bags by name' do
+      lot1 = Lot.create!(name: 'honduras', importer: 'lamanita', lot_number: 55, organic: false)
+      bag1 = lot1.bags.create!(name: 'a', ground: true, size: 340)
+      bag2 = lot1.bags.create!(name: 'dark', ground: true, size: 60)
+      bag3 = lot1.bags.create!(name: 'a', ground: true, size: 60)
+      expect(lot1.bags_sorted_by_name).to eq([bag1, bag3, bag2])
+    end
   end
 end
